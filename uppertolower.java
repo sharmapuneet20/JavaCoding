dd06cd01608e4974f86d2a2f7f7f58ee2719f3fe
sharmapuneet20 <107070137+sharmapuneet20@users.noreply.github.com>
2023-05-06 12:19:05 +0530

    Add files via upload

---
+++ uppertolower.java
@@ -0,0 +1,28 @@
+public class uppertolower {
+    public static String touppercase(String str){
+        StringBuilder sb = new StringBuilder("");
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for (int i=1; i<str.length(); i++){
+            if (str.charAt(i) ==' ' && i<str.length()-1){
+//                space copied
+                sb.append(str.charAt(i));
+//                next letter
+
+                i++;
+//                converted to uppercase
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+
+    }
+    public static void main(String[] args) {
+        String str = "hi, i am puneet";
+        System.out.println(touppercase(str));
+
+    }
+}
